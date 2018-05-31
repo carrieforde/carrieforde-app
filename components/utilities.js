ba6a763b383cc8350389946012e230080f4ce2e4
Carrie Forde <carrie@carrieforde.com>
2018-05-30 18:58:41 -0700

    Removes unneeded files

--- components/utilities.js
+++
@@ -1,9 +0,0 @@
-class Utilities {
-  getData(endpoint = 'posts', cb) {
-    fetch(`http://fordeapp.test/wp-json/wp/v2/${endpoint}`)
-      .then(response => response.json())
-      .then(myJson => cb(myJson));
-  }
-}
-
-export default Utilities;
